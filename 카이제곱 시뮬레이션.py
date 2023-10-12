6ffef9c2355df76e641fdd9b0ae3396abc63e98f
Han Kyoung Min <48062593+alsh02@users.noreply.github.com>
2023-10-12 10:25:47 +0900

    Update 카이제곱 시뮬레이션.py

--- 카이제곱 시뮬레이션.py
+++ 카이제곱 시뮬레이션.py
@@ -6,15 +6,22 @@ v_values = [1, 2, 3, 10]
 k_values = [50, 100, 500, 1000]
 
 plt.style.use("ggplot")
+plt.figure(figsize=(15, 8))
 
-for v in v_values:
-    for k in k_values:
+location = 1
+for vidx, v in enumerate(v_values):
+    for kidx, k in enumerate(k_values):
         chi2_dist = np.array([])
         for i in range(k):
             samples = norm.rvs(size=v, loc=0, scale=1)
             chi2_dist = np.append(chi2_dist, (samples ** 2).sum())
         x = np.linspace(0, max(chi2_dist), 1000)
+        
+        plt.subplot(4, 4, location)
         plt.title(f"v={v}, k={k}")
         plt.plot(x, chi2.pdf(x, v), color='b', linewidth=2)
         plt.hist(chi2_dist, density=True)
-        plt.show()
+        location += 1
+
+plt.tight_layout()
+plt.show()
